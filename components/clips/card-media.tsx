ca506cec0ba98c1d1f1195469eb2ea31c4699865
Sygeman <sygeman@gmail.com>
2023-10-18 20:33:27 +0300

    card fix

--- components/clips/card-media.tsx
+++ components/clips/card-media.tsx
@@ -1,37 +1,40 @@
 "use client";
 
-import React from "react";
+import { type ReactNode } from "react";
 
 import { shortNumbers } from "@/utils/count";
 
 interface CardMediaProperties {
-  media?: React.ReactNode;
+  media?: ReactNode;
   avatar?: string;
   title?: string;
   description?: string;
   descriptionLink?: string;
-  overlay?: React.ReactNode;
+  overlay?: ReactNode;
   count?: number;
-  countIcon?: React.ReactNode;
+  countIcon?: ReactNode;
 }
 
-export const CardMedia: React.FC<CardMediaProperties> = ({
+export const CardMedia = ({
   media,
   title,
   overlay,
   count = 0,
-}) => (
+}: CardMediaProperties) => (
   <div className="flex flex-col items-center rounded overflow-hidden relative">
     <div className="relative w-full aspect-video bg-surface/50">{media}</div>
     <div className="flex bg-surface w-full h-12 px-1 text-sm">
-      <div className="flex flex-1 h-full items-center px-2">
-        <div className="truncate w-full">{title}</div>
+      <div
+        className="flex flex-1 h-full items-center px-2 truncate"
+        title={title}
+      >
+        {title}
       </div>
       <div className="flex justify-center items-center h-full px-2">
         <span
           className={
             "text-white/75 px-2 py-1 rounded font-medium " +
-            `${count > 0 ? "bg-twitch" : "bg-background"}`
+            `${count > 0 ? "bg-primary" : "bg-background"}`
           }
         >
           {shortNumbers(count)}
