7e8212f08705dba394fb3348c6cb0dd2a51c3096
Sygeman <sygeman@gmail.com>
2023-10-29 16:48:22 +0300

    upd

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["static-cdn.jtvnw.net"],
   },
+  experimental: {
+    serverActions: true,
+  },
   output: "standalone",
   reactStrictMode: true,
 };
